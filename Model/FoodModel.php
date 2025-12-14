63291c47842d2bf41dbcbab7e1514b02126bc883
Huy <dotruong085@gmail.com>
2025-12-14 09:56:41 +0700

    Bổ sung thêm hàm getall lấy data vào foodmodel

--- Model/FoodModel.php
+++ Model/FoodModel.php
@@ -2,7 +2,7 @@
 include_once '../core/BaseModel.php';
 class FoodModel extends BaseModel
 {
-        private const TB_NAME = "foods";
+        public const TB_NAME = "foods";
         protected $food_id;
         protected $category_id;
         protected $food_name;
@@ -12,9 +12,43 @@ class FoodModel extends BaseModel
         protected $status;
         protected $created_at;
 
-        public function __construct()
+        public function __construct($food_id = null,  $category_id = null,  $food_name = null,  $description = null,  $price = null,  $image_url = null,  $status = null)
         {
                 parent::__construct(self::TB_NAME);
+                $this->food_id = $food_id;
+                $this->category_id = $category_id;
+                $this->food_name = $food_name;
+                $this->description = $description;
+                $this->price = $price;
+                $this->image_url = $image_url;
+                $this->status = $status;
+        }
+
+        public function getAll()
+        {
+                try {
+                        $list_foods = [];
+                        $stmt = $this->db->query("SELECT * FROM " . self::TB_NAME . " join category on " . self::TB_NAME . ".category_id = category.category_id WHERE status = 1");
+                        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                        while ($row = $stmt->fetch()) {
+                                $list_foods[] = new FoodModel($row['food_id'], $row['category_id'], $row['food_name'], $row['description'], $row['price'], $row['image_url'], $row['status']);
+                        }
+                        return $list_foods;
+                } catch (PDOException $e) {
+                        $errorCode = isset($e->errorInfo[1]) ? $e->errorInfo[1] : 0;
+                        switch ($errorCode) {
+                                case 1054:
+                                        $this->error_message = "Lỗi SQL: Tên cột không tồn tại.";
+                                        break;
+                                case 1146:
+                                        $this->error_message = "Lỗi SQL: Bảng không tồn tại.";
+                                        break;
+                                default:
+                                        $this->error_message = "Lỗi truy vấn dữ liệu: " . $e->getMessage();
+                                        break;
+                        }
+                        return [];
+                }
         }
 
         public function getFood_id()
