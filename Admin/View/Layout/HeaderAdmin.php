3be7cc8ee95880412356701856c8b6f1ec2f0a29
Huy <dotruong085@gmail.com>
2025-12-16 09:51:27 +0700

    Cập nhật lại header active menu

--- Admin/View/Layout/HeaderAdmin.php
+++ Admin/View/Layout/HeaderAdmin.php
@@ -59,8 +59,8 @@
         </div>
     </div>
     <div class="navbar-nav w-100">
-        <a href="index.php?page=HomeAdmin" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Trang chủ</a>
-        <a href="index.php?page=FoodAdmin" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Quản lý món ăn</a>
+        <a href="index.php?page=HomeAdmin" class="nav-item nav-link <?= ($page == 'HomeAdmin') ? 'active' : '' ?>"><i class="fa fa-tachometer-alt me-2"></i>Trang chủ</a>
+        <a href="index.php?page=FoodAdmin" class="nav-item nav-link <?= ($page == 'FoodAdmin') ? 'active' : '' ?>"><i class="fa fa-th me-2"></i>Quản lý món ăn</a>
         <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
             <div class="dropdown-menu bg-transparent border-0">
