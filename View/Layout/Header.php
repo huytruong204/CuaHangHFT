d3ffdaaf13c1d5ac4635797cb341b3e9daa0368f
Thinh <nqtlop91@gmail.com>
2025-12-16 10:39:06 +0700

    Sửa header trong layout, index in home, css

--- View/Layout/Header.php
+++ View/Layout/Header.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Online Food</title>
-	<base href="http://localhost/baitapPHP/CuaHangHFT/">
 	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="assets/css/global.css" rel="stylesheet">
 	<link href="assets/css/index.css" rel="stylesheet">
@@ -31,7 +30,7 @@
  <div class="main_1 clearfix">
   <section id="menu" class="clearfix cd-secondary-nav">
 	<nav class="navbar nav_t">
-		<div class="container">
+		<div class="container-fluid">
 		    <div class="navbar-header page-scroll">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle navigation</span>
@@ -39,23 +38,33 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="index.php?page=Home"> <i class="fa fa-spoon col_1"></i> Online Food <br> <span class="small_tag">Restaurant</span> </a>
+				<a class="navbar-brand" href="index.php?page=Home"> <i class="fa fa-cutlery"></i> HFT <br> </a>
 			</div>
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav nav_m navbar-right">
-				<li><a class="m_tag active_tab" href="index.php?page=Home">Home</a></li>
+				<ul class="nav navbar-nav nav_m">
+				<li><a class="m_tag active_tab" href="index.php?page=Home">Trang chủ</a></li>
 				<li class="dropdown">
-					  <a class="m_tag" href="#" data-toggle="dropdown" role="button" aria-expanded="false">Shop<span class="caret"></span></a>
+					  <a class="m_tag" href="shop.html" data-toggle="dropdown" role="button" aria-expanded="false">Thực đơn<span class="caret"></span></a>
 					  <ul class="dropdown-menu drop_3" role="menu">
-						<li><a href="shop.html">Shop</a></li>
-						<li><a class="border_none" href="detail.html">Shop Detail</a></li>
+						<li><a href="#">Gà rán</a></li>
+						<li><a href="#">Mì ý</a></li>
+						<li><a href="#">Burger</a></li>
+						<li><a href="#">Cơm</a></li>
+						<li><a href="#">Phần ăn phụ</a></li>
+						<li><a href="#">Tráng miệng</a></li>
+						<li><a class="border_none" href="#">Đồ uống</a></li>
 					  </ul>
                 </li>
-			    <li><a class="m_tag" href="index.php?page=Contact">Contact Us</a></li>
-				<li class="m_tag2 col"><i class="fa fa-phone col_1"></i> +123-456 7899 <br> <span class="normal small_tag">Contact us for reservation</span></li>
-				<li><a class="m_tag1 button mgt" href="reserve.html"> BOOK A TABLE</a></li>
+			    <li><a class="m_tag" href="#">Khuyến mãi</a></li>
+				<li><a class="m_tag" href="#">Dịch vụ</a></li>
+				<li><a class="m_tag" href="index.php?page=Contact">Liên hệ</a></li>
+				<li><a class="m_tag1 button mgt" href="#"> Giỏ hàng</a></li>
+			</ul>
+			<ul class="nav navbar-nav nav_m navbar-right">
+				<li><a class="m_tag1 button mgt" href="index.php?page=Login"> Đăng nhập</a></li>
+				<li><a class="m_tag1 button mgt" href="index.php?page=Register"> Đăng ký</a></li>
 			</ul>
 			    
 			</div>
