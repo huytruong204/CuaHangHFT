526abcf6e0e194fca7b06374e7fafa5b18b22e3d
Huy <dotruong085@gmail.com>
2026-01-07 14:27:50 +0700

    Sửa trang liên hệ

--- View/Contact/Index.php
+++ View/Contact/Index.php
@@ -12,84 +12,53 @@
   <div class="container">
     <div class="row">
       
-      <div class="col-sm-8">
+      <div class="col-sm-12">
         <div class="card-box">
-          <h3 class="card-title">Gửi tin nhắn cho chúng tôi</h3>
-          <div class="contact_form">
-            <div class="row">
-               <div class="col-sm-6 space_left">
-                  <h5>Họ và tên <span class="col_3" style="color:red">*</span></h5>
-                  <input class="form-control" type="text" placeholder="Nhập tên của bạn...">
-               </div>
-               <div class="col-sm-6 space_left">
-                  <h5>Chủ đề <span class="col_3" style="color:red">*</span></h5>
-                  <input class="form-control" type="text" placeholder="Vấn đề cần hỗ trợ...">
-               </div>
-            </div>
-            <br>
-            <div class="row">
-               <div class="col-sm-6 space_left">
-                  <h5>Email <span class="col_3" style="color:red">*</span></h5>
-                  <input class="form-control" type="email" placeholder="Nhập email...">
-               </div>
-               <div class="col-sm-6 space_left">
-                  <h5>Số điện thoại <span class="col_3" style="color:red">*</span></h5>
-                  <input class="form-control" type="text" placeholder="Nhập số điện thoại...">
-               </div>
-            </div>
-            <br>
-            <div class="row">
-               <div class="col-sm-12 space_left">
-                  <h5>Nội dung lời nhắn <span class="col_3" style="color:red">*</span></h5>
-                  <textarea class="form-control form_1" placeholder="Nhập nội dung..."></textarea>
-                  <br>
-                  <h6><a class="btn-orange" href="#">Gửi tin nhắn</a></h6>
-               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-4">
-        <div class="card-box">
-           <h3 class="card-title">Thông tin liên hệ</h3>
+           <h3 class="card-title" style="text-align: center; margin-bottom: 30px;">Thông tin liên hệ</h3>
            
-           <div class="info-item">
-              <div class="info-icon"><i class="fa fa-phone"></i></div>
-              <div class="info-content">
-                 <h5>Điện thoại:</h5>
-                 <p>0123 456 789</p>
-                 <p>0987 654 321</p>
-              </div>
-           </div>
+           <div class="row">
+               <div class="col-sm-4">
+                   <div class="info-item" style="text-align: center;">
+                      <div class="info-icon" style="font-size: 30px; margin-bottom: 15px;"><i class="fa fa-phone"></i></div>
+                      <div class="info-content">
+                         <h5>Điện thoại:</h5>
+                         <p>0123 456 789</p>
+                         <p>0987 654 321</p>
+                      </div>
+                   </div>
+               </div>
 
-           <div class="info-item">
-              <div class="info-icon"><i class="fa fa-envelope"></i></div>
-              <div class="info-content">
-                 <h5>Email:</h5>
-                 <p>cskh@hftfood.com</p>
-                 <p>lienhe@gmail.com</p>
-              </div>
-           </div>
+               <div class="col-sm-4">
+                   <div class="info-item" style="text-align: center;">
+                      <div class="info-icon" style="font-size: 30px; margin-bottom: 15px;"><i class="fa fa-envelope"></i></div>
+                      <div class="info-content">
+                         <h5>Email:</h5>
+                         <p>cskh@hftfood.com</p>
+                         <p>lienhe@gmail.com</p>
+                      </div>
+                   </div>
+               </div>
 
-           <div class="info-item">
-              <div class="info-icon"><i class="fa fa-map-marker"></i></div>
-              <div class="info-content">
-                 <h5>Địa chỉ:</h5>
-                 <p>123 Đường ABC, Quận XYZ</p>
-                 <p>Thành phố Hồ Chí Minh</p>
-              </div>
-           </div>
-        </div>
+               <div class="col-sm-4">
+                   <div class="info-item" style="text-align: center;">
+                      <div class="info-icon" style="font-size: 30px; margin-bottom: 15px;"><i class="fa fa-map-marker"></i></div>
+                      <div class="info-content">
+                         <h5>Địa chỉ:</h5>
+                         <p>123 Đường ABC, Quận XYZ</p>
+                         <p>Thành phố Hồ Chí Minh</p>
+                      </div>
+                   </div>
+               </div>
+           </div> </div>
       </div>
 
     </div>
 
     <div class="row">
        <div class="col-sm-12">
-          <div class="card-box" style="padding: 10px;">
-             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.494572234!2d106.6!3d10.7!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDQ2JzEwLjgiTiAxMDbCsDQwJzU0LjEiRQ!5e0!3m2!1svi!2s!4v1" 
-             width="100%" height="400" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy"></iframe>
+          <div class="card-box" style="padding: 10px; margin-top: 20px;">
+             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4945195000000!2d106.697!3d10.776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDQ2JzM2LjAiTiAxMDbCsDQxJzQ5LjIiRQ!5e0!3m2!1svi!2s!4v1600000000000!5m2!1svi!2s" 
+             width="100%" height="450" style="border:0; border-radius: 10px;" allowfullscreen="" loading="lazy"></iframe>
           </div>
        </div>
     </div>
